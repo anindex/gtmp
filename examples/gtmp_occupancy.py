3bcee57faed58a83ddca87c60251d71cc9516fd7
anindex <an.thai.le97@gmail.com>
2025-01-15 19:05:05 +0100

    Add persistent XLA compilation cache
    Signed-off-by: anindex <an.thai.le97@gmail.com>

--- examples/gtmp_occupancy.py
+++ examples/gtmp_occupancy.py
@@ -12,6 +12,11 @@ from gtmp.planners import GTMPState, gtmp_plan, gtmp_akima_plan
 from gtmp.objectives.occupancy_map import OccupancyMap
 from gtmp.metrics import compute_metrics
 
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
+
 
 @hydra.main(version_base=None, config_path=get_configs_path().as_posix(), config_name="demo_gtmp_occupancy")
 def main(cfg: omegaconf.DictConfig):
